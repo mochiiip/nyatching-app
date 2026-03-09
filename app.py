15c51f3cba6e1e3c62a9d8f7c32f2b6a029c2a64
mochiiip <norigep@gmail.com>
2026-03-09 19:35:46 +0900

    Update app.py

--- app.py
+++ app.py
@@ -5,13 +5,18 @@ st.title("ニャッチング診断")
 
 cats = pd.read_csv("cats_info.csv")
 
-for i, cat in cats.iterrows():
+for _, cat in cats.iterrows():
     st.subheader(cat["name"])
-    st.image("images/" + cat["image"])
+
+    st.image("images/" + cat["image"], width=300)
+
     st.write("年齢:", cat["age"])
     st.write("性別:", cat["sex"])
+    st.write("性格:", cat["personality"])
     st.write("人慣れ:", cat["friendly"])
     st.write("お留守番OK:", cat["alone_ok"])
     st.write("子どもOK:", cat["kids_ok"])
     st.write("他の猫OK:", cat["other_cats_ok"])
     st.write("猫エイズ:", cat["is_fiv_positive"])
+
+    st.divider()
