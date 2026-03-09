7a4d0a15ba2d627862f4462905930fa58af92b0e
mochiiip <norigep@gmail.com>
2026-03-09 19:30:54 +0900

    Create app.py

---
+++ app.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import pandas as pd
+
+st.title("ニャッチング診断")
+
+cats = pd.read_csv("cats_info.csv")
+
+for i, cat in cats.iterrows():
+    st.subheader(cat["name"])
+    st.image("images/" + cat["image"])
+    st.write("年齢:", cat["age"])
+    st.write("性別:", cat["sex"])
+    st.write("人慣れ:", cat["friendly"])
+    st.write("お留守番OK:", cat["alone_ok"])
+    st.write("子どもOK:", cat["kids_ok"])
+    st.write("他の猫OK:", cat["other_cats_ok"])
+    st.write("猫エイズ:", cat["is_fiv_positive"])
